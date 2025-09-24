1e5c64b40847155e0e5a1b1523c601cdce58be2a
Corneilleb01 <corneilleboshab9@gmail.com>
2025-09-24 18:09:50 +0100

    feat: adding new characters

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,7 @@
-// Exercice 6
-// Écrivez votre code ici
+
+let age = 20; // valeur numérique
+let nom = "Corneille"; // chaîne de caractères
+let estConnecte = true; // valeur booléenne
+let valeurNulle = null; // valeur nulle
+let inconnue; // variable sans valeur (undefined)
+
