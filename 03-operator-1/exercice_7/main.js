5d76170898ca4fe5de8c261fe04114555973cd2a
Corneilleb01 <corneilleboshab9@gmail.com>
2025-09-24 18:35:56 +0100

    feat: adding new exercices

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,13 @@
-// Exercice 7
-// Écrivez votre code ici
+
+let ageEnfant = 20; // valeur de départ
+
+let agePere = ageEnfant * 2; // double de l'âge de l'enfant
+
+let ageMere = agePere - 5; // âge du père moins 5
+
+let ageGrandPere = (ageMere * 2) + (ageEnfant / 2); // double de l'âge de la mère + moitié de l'âge de l'enfant
+
+let ageOncle = agePere + 10; // âge du père plus 10
+
+console.log(ageEnfant, agePere, ageMere, ageGrandPere, ageOncle);
+
